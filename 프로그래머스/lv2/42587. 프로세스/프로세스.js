456acd92bd27abdbfaa31a56efd8bd12393dc3a9
sincerity developer <85999976+jisung24@users.noreply.github.com>
2023-06-13 14:58:38 +0900

    [level 2] Title: 프로세스, Time: 0.32 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프로세스/프로세스.js
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.js
@@ -1,67 +1,63 @@
-'use strict';
 class Queue {
-    constructor() {
-        this.queue = [];
-        this.front = 0;
-        this.rear = 0;
-    }
-    
-    enqueue(value) {
-        this.queue[this.rear++] = value;
-    }
-    
-    dequeue() {
-        let value = this.queue[this.front];
-        delete this.queue[this.front]; // 값을 실제로 삭제해준다.
-        this.front += 1;
-        
-        return value;
-    }
-    
-    peek() {
-        return this.queue[this.front]; // 가장 먼저 빠질 요소!
-    }
-    
-    size() {
-        return this.rear - this.front;
-    }
+  constructor() {
+    this.queue = [];
+    this.front = 0;
+    this.rear = 0;
+  }
+  // { queue : [], front : 0, rear : 0 }
+
+  enqueue(value) {
+    this.queue[this.rear++] = value;
+  }
+
+  dequeue() {
+    let value = this.queue[this.front];
+    delete this.queue[this.front];
+    this.front += 1;
+    return value;
+  }
+
+  peek() {
+    return this.queue[this.front];
+  }
+
+  size() {
+    return this.rear - this.front;
+  }
 }
 
-//  {"queue":[[2,0],[1,1],[3,2],[2,3]],"front":0,"rear":4}
+
 function solution(arr, location) {
-    // 큐에서 빠질 차례가 아니라면, 다시 넣어준다. 
-    
-    // 빠질 차례가 됨. (몇 등으로 빠지는지 => count에 담는다! )
-    // 1. location과 같지 않다면, count += 1;
-    // 2. location과 같다면(내 차례라면) count출력!
-    
-    // 여기서 하나하나 해준다. 
-    let maxArr = [ ...arr ].sort((a, b) => {
+    // 어떤 값이 나가야하는지, 비교해주기 위함!
+    let count = 0;
+    let max = [ ...arr ].sort((a, b) => {
         if(a > b) return -1;
         else if(a < b) return 1;
     })
-    let index = 0;
-    let count = 0; // 몇 번째로 나가는지! (변수)
-    let queue = new Queue(); // 큐 생성! 
+    
+    // 큐 생성 후 초기화! 
+    let queue = new Queue(); 
     for(let i = 0; i < arr.length; i++) {
-        queue.enqueue([arr[i], i]); 
+        queue.enqueue([arr[i], i]);
     }
     
     while(queue.size() > 0) {
-        if(maxArr[index] > queue.peek()[0]) {
-            // 아직 나가면 안 돼!!
+        if(queue.peek()[0] < max[count]) {
+            // 못 나감! 
             queue.enqueue(queue.dequeue()); 
         }else{
-            // 나가도 돼! 
+            // 나갈 수 있는데...! 
             let value = queue.dequeue();
             if(value[1] === location) {
+                // 내가 나갈 차례라면...! 
                 count += 1;
                 return count;
             }else{
-                // 내 차례가 아니라면! 
+                // 내 차례가 아니라면..! 
                 count += 1;
-                index += 1; // 일단 나갔으니까 index를 올려줌
             }
         }
     }
+    
+    return count
 }
