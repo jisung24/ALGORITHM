3d2c995c95c627b5a812a52e6617187e6a9e95cd
sincerity developer <85999976+jisung24@users.noreply.github.com>
2023-06-18 16:18:50 +0900

    [level 2] Title: 타겟 넘버, Time: 1.06 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,20 +1,18 @@
-function solution(numbers, target) {
-  let answer = 0;
-  const length = numbers.length;
-
-  function dfs(count, sum) {
-    if (count === length) {
-      if (target === sum) {
-        answer++;
-      }
-      return;
+function solution(numbers, target){
+    // 숫자 배열, 합하거나 빼서 나올 수
+    let answer = 0;
+    function dfs(level, sum) {
+        if(level === numbers.length){
+            if(target === sum) answer += 1; 
+            // answer은 같을 경우의 수! 
+            else return; // undefined! 
+        }else{
+            // 아직 연산이 남았을 경우! 
+            // 재귀호출 부분! 
+            dfs(level + 1, sum + numbers[level]);
+            dfs(level + 1, sum - numbers[level]);
+        }
     }
-
-    dfs(count + 1, sum + numbers[count]);
-    dfs(count + 1, sum - numbers[count]);
-  }
-
-  dfs(0, 0);
-
-  return answer;
+    dfs(0, 0);
+    return answer;
 }
