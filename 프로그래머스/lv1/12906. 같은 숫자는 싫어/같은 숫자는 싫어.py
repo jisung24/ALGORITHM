7e570fde1372ce00b40b5f07eae7ffb9ba18df65
sincerity developer <85999976+jisung24@users.noreply.github.com>
2023-06-22 15:44:36 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 168.99 ms, Memory: 27.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,10 +1,10 @@
+# 1대 다인지, 그냥 1대 1인지! => 선형 비선형 자료구조 
 def solution(arr):
     stack = [arr[0]]
+    
     for i in range(1, len(arr)):
-        if stack[-1] != arr[i]:
+        if arr[i] != stack[-1]:
+            # 만약 arr의 원소가 stack마지막 원소와 다르다면 
             stack.append(arr[i])
             
     return stack
-
-
-    
