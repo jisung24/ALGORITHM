5da3f9622c88f2e5fd10f17252ea4d53453c1536
sincerity developer <85999976+jisung24@users.noreply.github.com>
2023-09-28 01:56:24 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,10 +1,9 @@
-function solution(n){
-    if(typeof n !== 'number') throw new Error("자연수가 필요합니다!")
-    return Array.from(String(n), Number).reduce((acc, cur) => acc + cur);
+// 자릿수 더하기 
+// 1. 쪼개기
+// 2. 합 구하기 
+const sumOfArray = (arr) => arr.reduce((acc, cur) => acc + cur)
+
+const solution = (N) => {
+    const numberArray = Array.from(String(N), Number);
+    return sumOfArray(numberArray);
 }
-
-
-// Array.from(배열로 바꾸기 위한 이터러블, 유사배열 객체)
-// 1. 이터러블 : 순차적으로 반복문 사용해서 접근할 수 있는 객체 => ex) 배열, 문자열, map, set 등등... 
-// -> for of나 다른 반복문 사용해서 접근이 가능하다. 
-// 2. 유사배열객체 
