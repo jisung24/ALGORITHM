b270dfe64072d643e8cb84ca6bd2ddeddb63c9ca
sincerity developer <85999976+jisung24@users.noreply.github.com>
2023-09-27 17:45:10 +0900

    [level 1] Title: 약수의 합, Time: 0.16 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
@@ -1,8 +1,11 @@
 function solution(number) {
-    let sum = 0;
-    for(let i = 1; i <= number; i++){
+    let sum = 1;
+    if(typeof number !== 'number') throw new Error("자연수가 와야합니다...!");
+    if(number === 0 || number === 1) return number;
+    // 0과 1은 제외함 위에서! 
+    for(let i = 2; i <= number; i++) {
         if(number % i === 0) sum += i;
     }
     
-    return sum
+    return sum;
 }
