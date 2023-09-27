dd32ae9e170d52caee5ac6478548fd35311e0c00
sincerity developer <85999976+jisung24@users.noreply.github.com>
2023-09-28 02:03:52 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.js
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.js
@@ -1,3 +1,7 @@
-function solution(number) {
-    return number % 2 === 0 ? "Even" : "Odd"
+// 1. 짝수이면 Even, 홀수이면 Odd 
+
+// - 짝수, 홀수 판별해주는 함수! 
+const isEven = (number) => number % 2 === 0; 
+const solution = (number) => {
+    return isEven(number) ? "Even" : "Odd";
 }
