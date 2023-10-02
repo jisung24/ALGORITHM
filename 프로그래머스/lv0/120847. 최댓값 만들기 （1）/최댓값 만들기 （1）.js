511e84bf7e80e67c686aa13cbd717d6cf4f5dbfc
sincerity developer <85999976+jisung24@users.noreply.github.com>
2023-10-03 08:03:18 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 3.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -1,9 +1,9 @@
-// 2개를 곱해서 만들 수 있는 최댓값을 return
+// 그냥 가장 큰 2개의 수 곱하면 됨
 function solution(numbers) {
-    const sortedNumbers = numbers.sort((a, b) => {
-        if(a > b) return -1;
-        if(a < b) return 1;
+    const sortedArr = numbers.sort((a, b) => {
+        if(a < b) return -1;
+        if(a > b) return 1;
     })
-    
-    return sortedNumbers[0] * sortedNumbers[1];
+    console.log(sortedArr);
+    return sortedArr.at(-1) * sortedArr.at(-2);
 }
