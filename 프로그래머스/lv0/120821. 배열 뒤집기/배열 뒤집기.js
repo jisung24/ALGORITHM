05dd3ae0dd83f133bbcdac545c67732c16762fec
sincerity developer <85999976+jisung24@users.noreply.github.com>
2023-10-03 07:45:13 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -1,3 +1,9 @@
+// 1. reverse 
+// 2. stack
 function solution(num_list) {
-    return num_list.reverse();
+    const stack = [];
+    for(let i = num_list.length - 1; i >= 0; i--){
+        stack.push(num_list[i]);
+    }
+    return stack;
 }
