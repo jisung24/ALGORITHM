be4be766963a41f70ec404acb0f5dc23c2160a49
sincerity developer <85999976+jisung24@users.noreply.github.com>
2023-10-04 01:32:51 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -2,8 +2,11 @@ const checkHasZero = (dot) => {
     return dot.some(e => e === 0);
 }
 
+const checkDotsRange = (dot) => {
+    return (!checkHasZero(dot)) && dot.every(e => (e >= -500 && e <= 500))
+}
 function solution(dot) {
-    if(checkHasZero(dot)) throw new Error("0이 포함 돼 있어서 이상한 좌표입니다!");
+    if(!checkDotsRange(dot)) throw new Error("예외 발생!");
     const [dotX, dotY] = dot;
     
     if(dotX > 0 && dotY > 0) return 1; 
