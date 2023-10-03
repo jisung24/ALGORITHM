ddc7d7ee1fd36450c36a69f11879885e2b294738
sincerity developer <85999976+jisung24@users.noreply.github.com>
2023-10-03 15:55:32 +0900

    [level 1] Title: 음양 더하기, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -1,9 +1,11 @@
-function solution(absolutes, signs) {
+const sumOfValues = (absolutes, signs) => {
     let sum = 0;
     for(let i = 0; i < signs.length; i++) {
         if(signs[i]) sum += absolutes[i];
-        else if(!signs[i]) sum -= absolutes[i];
+        if(!signs[i]) sum -= absolutes[i];
     }
-    
     return sum;
+}
+function solution(absolutes, signs) {
+    return sumOfValues(absolutes, signs);
 }
