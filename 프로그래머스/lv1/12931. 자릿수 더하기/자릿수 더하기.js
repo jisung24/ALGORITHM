7d1941569944da618151b5c52833a103e69b7e3b
sincerity developer <85999976+jisung24@users.noreply.github.com>
2023-10-03 14:44:26 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,9 +1,16 @@
-// 자릿수 더하기 
-// 1. 쪼개기
-// 2. 합 구하기 
-const sumOfArray = (arr) => arr.reduce((acc, cur) => acc + cur)
+const checkNumberType = arr => arr.some(e => typeof e !== 'number');
+const getSumOfNumbers = (arr) => {
+    if(checkNumberType(arr)) throw new Error("배열은 전부 number type이어야 합니다!");
+    return arr.reduce((acc, cur) => acc + cur, 0);
+} 
 
-const solution = (N) => {
-    const numberArray = Array.from(String(N), Number);
-    return sumOfArray(numberArray);
+function solution(n){
+    if(typeof n !== 'number') throw new Error("자연수가 필요합니다!")
+    return getSumOfNumbers(Array.from(String(n), Number));
 }
+
+// Array.from 첫 번째 파라미터 안에는 배열로 바꿀 이터러블 한 객체가 들어가야한다. 
+// 하지만 지금 첫 번째 파라미터는 배열로 바꿀 이터러블 객체가 없고,
+// 자연수가 있다. => 문자열로 바꿔서 넣어준다
+
+// 2번째 파라미터 : 어떤 타입으로 넣을 것인가.. 
