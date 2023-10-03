46ce0c811c3ceea9af749fe16376261e1fa80677
sincerity developer <85999976+jisung24@users.noreply.github.com>
2023-10-03 16:35:05 +0900

    [level 2] Title: 올바른 괄호, Time: 8.31 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,15 +1,12 @@
-function solution(s){
-    // 괄호 => 가장 마지막에 추가된 열린 괄호부터 닫혀야돼! => 마지막이 먼저? => stack 
-    // 숫자로 한 번 풀어볼게 => 메모리 절약 가능! 
-    let stack = 0;
-    
+const solution = (s) => {
+    const stack = [];
     for(let i = 0; i < s.length; i++) {
-        if(s[i] === '(') stack += 1; // 1
-        else{
-            if(stack === 0) return false;
-            else stack -= 1;
+        if(s[i] === '(') stack.push(s[i]); // 무조건 넣고..! 
+        else {
+            if(stack.length === 0) return false;
+            stack.pop();
         }
     }
     
-    return stack === 0;
+    return stack.length === 0;
 }
