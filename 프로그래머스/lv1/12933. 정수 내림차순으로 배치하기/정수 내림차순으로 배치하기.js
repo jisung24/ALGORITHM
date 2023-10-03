8ed15c5cbe9879cc84fac3440262407e60112274
sincerity developer <85999976+jisung24@users.noreply.github.com>
2023-10-03 15:23:18 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.js
@@ -1,8 +1,12 @@
-function solution(n) {
-    return Number(Array.from(String(n), Number)
+// 1. 문자열 -> 배열 -> 숫자 한 번에 묶기 (문자열을 숫자타입 배열로 바꿔주기!)
+const solution = (n) => {
+    return Number(n.toString()
+    .split('')
+    .map(Number)
     .sort((a, b) => {
         if(a > b) return -1;
         if(a < b) return 1;
+        return 0;
     })
-    .join(''));
+    .join(''))
 }
