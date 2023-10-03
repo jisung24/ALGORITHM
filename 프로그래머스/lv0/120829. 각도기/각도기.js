75b9ddf3e2ecdfcc5220ae6b3514d4e76db56f1c
sincerity developer <85999976+jisung24@users.noreply.github.com>
2023-10-04 00:45:23 +0900

    [level 0] Title: 각도기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.js
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -1,8 +1,15 @@
-// 어떤 각인지 알려주는 함수! 
-function solution(angle) {
-    if(0 < angle && angle < 90) return 1;
+const checkAngleRange = (angle) => {
+    return Number.isInteger(angle)
+    && angle > 0 && angle <= 180;
+}
+
+const getAngleType = (angle) => {
+   if(!checkAngleRange(angle)) throw new Error("wrong angle value")
+    if(angle > 0 && angle < 90) return 1;
     if(angle === 90) return 2;
     if(angle > 90 && angle < 180) return 3;
     if(angle === 180) return 4;
-    // 예외는 문제에서 없다고 나옴!
+}
+function solution(angle) {
+    return getAngleType(angle);
 }
