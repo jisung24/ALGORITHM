f5749ab5d5d31a626b81f4f8a7c2798ca0e5ebf5
sincerity developer <85999976+jisung24@users.noreply.github.com>
2023-10-04 00:24:52 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.js
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -1,4 +1,9 @@
-// 같은지 다른지 판단하는 함수! 
+const checkIsBiggerThanZero = (num1, num2) => num1 >= 0 && num2 >= 0;
+
+const checkType = (num1, num2) => {
+    return typeof num1 === 'number' && typeof num2 === 'number'
+}
 function solution(num1, num2) {
+    if(!checkIsBiggerThanZero(num1, num2) || (!checkType(num1, num2))) throw new Error("type error");
     return num1 === num2 ? 1 : -1;
 }
