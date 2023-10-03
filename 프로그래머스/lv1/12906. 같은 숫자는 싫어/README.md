c714b199c399a0b8f7be32daed46592c0d98e2a0
sincerity developer <85999976+jisung24@users.noreply.github.com>
2023-10-03 16:23:51 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 66.76 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/README.md
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 같은 숫자는 싫어 - 12906 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906?language=javascript) 
 
 ### 성능 요약
 
-메모리: 27.7 MB, 시간: 110.33 ms
+메모리: 83.2 MB, 시간: 66.76 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-Empty
+정확성: 71.9<br/>효율성: 28.1<br/>합계: 100.0 / 100.0
 
 ### 문제 설명
 
--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,11 +1,15 @@
-// 연속적으로 나타나는 숫자 제거 => 넣고, stack마지막 숫자랑 같으면 그냥 out! 
-function solution(arr)
-{
-    let stack = [arr[0]]; // 첫 번째 값은 넣고 시작.!
-    for(let i = 1; i < arr.length; i++) {
-        if(arr[i] !== stack[stack.length - 1]) stack.push(arr[i]);
-        // 즉 같지 않아야만 넣을 수 있어! => 가장 마지막에 있는 값과 비교하는게 stack이야! 
-        // 1. 현재 대기하는 값과 마지막값을 비교하는 경우, 즉 마지막 값에 따라서 현재 오는 값의 운명이 정해진다. 
+// 
+const checkSameWithLastNumber = (stack, number) => {
+    return stack.at(-1) === number;
+}
+
+const solution = (arr) => {
+    const stack = [];
+    for(let i = 0; i < arr.length; i++) {
+        if(stack.length === 0) stack.push(arr[i]); // 처음이면 넣고..! 
+        else{
+            if(!checkSameWithLastNumber(stack, arr[i])) stack.push(arr[i]);
+        }
     }
     
     return stack;
