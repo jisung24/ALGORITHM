3509a0f6112b72651a1d61b4eee9b5a8013a42b9
sincerity developer <85999976+jisung24@users.noreply.github.com>
2023-10-03 15:46:02 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -1,17 +1,13 @@
-function solution(a, b) {
-    // a와 b의 크기를 일단 정렬...!
-    if(a === b) return a;
-    const min = Math.min(...[a, b]);
-    const max = Math.max(...[a, b]);
-    let sum = min;
-    for(let i = min + 1; i <= max; i++) {
-        sum += i;
-    }
+const calculateSumBetween = (firstNumber, lastNumber) => {
+    const biggerNumber = Math.max(...[firstNumber, lastNumber]);
+    const smallerNumber = Math.min(...[firstNumber, lastNumber]);
+
+    let sum = 0; // 3 3일 때 sum을 smaller로 해버리면 3이 나옴 
+    for(let i = smallerNumber; i <= biggerNumber; i++) sum += i;
     
     return sum;
 }
 
-// 일단 문제! 
-// 1. a > b, b > a이렇게 나눠서 for문 2번쓰지 말고,
-// 아예 작은 수랑 큰 수를 구해서
-// 하나로 만들면 돼!
+const solution = (a, b) => {
+    return calculateSumBetween(a, b);
+}
