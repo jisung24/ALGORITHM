2b1d7dd097bf7177689efbf48cef077e0b32b6d0
sincerity developer <85999976+jisung24@users.noreply.github.com>
2023-10-04 00:29:46 +0900

    [level 0] Title: 나이 출력, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120820. 나이 출력/나이 출력.js
+++ 프로그래머스/lv0/120820. 나이 출력/나이 출력.js
@@ -1,4 +1,10 @@
-// 출생년도 구해주는 함수! 
+const checkIsNumber = (age) => {
+    return typeof age === 'number';
+}
+const getUserBirthYear = (age) => {
+   if(!checkIsNumber(age)) throw new Error("나이는 number type이어야합니다!")
+    return 2022 - age + 1; 
+}
 function solution(age) {
-    return 2022 - age + 1;
+    return getUserBirthYear(age);
 }
