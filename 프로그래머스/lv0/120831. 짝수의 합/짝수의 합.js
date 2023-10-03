775ffeee9246217b84a173d8493367bbb5f916e0
sincerity developer <85999976+jisung24@users.noreply.github.com>
2023-10-04 00:49:57 +0900

    [level 0] Title: 짝수의 합, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
@@ -1,7 +1,6 @@
-const solution = (n) => {
-    // 짝수의 합 
-    if(n === 0 || n === 1) return 0;
+function solution(n) {
     let sum = 0;
+    if(n === 1) return 0;
     for(let i = 2; i <= n; i++) {
         if(i % 2 === 0) sum += i;
     }
