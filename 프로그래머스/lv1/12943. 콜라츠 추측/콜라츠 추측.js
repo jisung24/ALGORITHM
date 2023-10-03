1448881a1133dc7eff5f18d6f556f695a2d191bd
sincerity developer <85999976+jisung24@users.noreply.github.com>
2023-10-03 15:50:50 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,17 +1,16 @@
-function solution(number) {
+const checkNumberType = number => typeof number === 'number';
+
+const solution = (number) => {
+    if(!checkNumberType(number)) throw new Error("숫자가 아닙니다!");
     if(number === 1) return 0;
-    
-    let count = 1;
-    while(count <= 500) { // 500번의 기회! 
-        if(number % 2 === 0) {
-            number /= 2;
-        }else number = number * 3 + 1;
-        // if if로 처리하면 뭐가 문제냐면... 
-        // if에서 연산이 끝났는데, 다음 if문을 만족시킬 수 있음 
+    let tryCount = 1;
+    while(tryCount <= 500) {
+        if(number % 2 === 0) number /= 2; // 어차피 짝수면 나눠떨어짐.
+        else if(number % 2 === 1) number = (number * 3) + 1
         
-        
-        if(number === 1) return count;
-        count += 1;
+        if(number !== 1) tryCount += 1;
+        else return tryCount;
     }
+    // 500번까지 안 됐을 때! 
     return -1;
 }
