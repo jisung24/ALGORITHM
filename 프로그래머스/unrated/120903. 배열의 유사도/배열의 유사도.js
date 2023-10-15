708f4615a3ebd5d825e7fdcae8722dbbefd5654e
sincerity developer <85999976+jisung24@users.noreply.github.com>
2023-10-15 20:16:47 +0900

    [level unrated] Title: 배열의 유사도, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/unrated/120903. 배열의 유사도/배열의 유사도.js
@@ -2,11 +2,6 @@
 // - 두 배열에서 같은 원소의 개수를 구하자! 
 // -1) 1개씩 find해보기...! => 
 function solution(s1, s2) {
-    let count = 0;
-    for(let i = 0; i < s2.length; i++) {
-        const findOne = s1.find(e => e === s2[i]);
-        if(findOne) count += 1; 
-    }
-    
-    return count;
+    // s1에 있으면서도 s2에 있는 원소들! 
+    return s1.filter(e => s2.includes(e)).length;
 }
