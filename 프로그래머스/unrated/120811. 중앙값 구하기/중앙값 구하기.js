795d1561c5bcd3f0cc3dcc686fb7f151dd7eb40f
sincerity developer <85999976+jisung24@users.noreply.github.com>
2023-10-23 10:19:56 +0900

    [level unrated] Title: 중앙값 구하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120811. 중앙값 구하기/중앙값 구하기.js
+++ 프로그래머스/unrated/120811. 중앙값 구하기/중앙값 구하기.js
@@ -1,6 +1,9 @@
-function solution(array) {
-    return array.sort((a, b) => {
-        if(a > b) return -1;
-        if(a < b) return 1;
-    })[parseInt(array.length / 2)]
-}
+const solution = (arr = []) => {
+        if(arr.length % 2 === 0) throw new Error("배열의 길이는 홀수여야합니다!");
+        const sortedIncArr = arr.sort((a, b) => {
+            if(a > b) return 1;
+            if(a < b) return -1;
+        })
+
+        return sortedIncArr[parseInt(sortedIncArr.length / 2)];
+    }
