83f1e32780ae1ec145bbaa0d8bbce1995ba3e634
sincerity developer <85999976+jisung24@users.noreply.github.com>
2023-11-25 19:43:30 +0900

    [level unrated] Title: 피자 나눠 먹기 (3), Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
+++ 프로그래머스/unrated/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
@@ -1,10 +1,8 @@
-// slice : 피자 조각 수 
-// n : 피자를 먹는 사람의 수
 function solution(slice, n) {
-    let pizzaCnt = 1;
-    while(slice * pizzaCnt < n){
-        pizzaCnt += 1;
-    }
-    
-    return pizzaCnt;
+  let pizza = 1;
+
+  while (slice * pizza < n) {
+    pizza += 1;
+  }
+  return pizza;
 }
