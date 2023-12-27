f0fed615e6b9ecc146ea69df55fe05651c56d599
sincerity developer <85999976+jisung24@users.noreply.github.com>
2023-12-27 15:31:33 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 13.74 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,13 +1,11 @@
-// 앞에 있는 값이랑 같으면... out! 
-// 가장 최근 값...! 
-// stack에 넣으면 돼! 
-// arr의 크기 => 최소 1 
+// stack: 넣었다 뺐다! 
+// 첫 번째 숫자는 그냥 넣고, index[1]부터 반복문 돌려서... 
+// 넣은 배열의 맨 뒤 숫자와 다르면 push하는걸로! 
 function solution(arr) {
-    const stack = [arr[0]] 
-    
+    const answer = [arr[0]]
     for(let i = 1; i < arr.length; i++) {
-        if(arr[i] !== stack[stack.length - 1]) stack.push(arr[i])
+        if(answer[answer.length - 1] !== arr[i]) answer.push(arr[i])
     }
     
-    return stack
+    return answer
 }
