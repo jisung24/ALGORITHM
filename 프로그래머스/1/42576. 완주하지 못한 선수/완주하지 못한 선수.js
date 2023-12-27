00ce6157c5d822bd688e5469e2e9a90f2c1fd05a
sincerity developer <85999976+jisung24@users.noreply.github.com>
2023-12-27 15:46:55 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 36.08 ms, Memory: 66.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,19 @@
+function solution(participant, completion) {
+    const map = new Map() // map객체 생성 
+    
+    // hash 정보 등록! 
+    for(let value of participant) {
+        if(!map.has(value)) map.set(value, 1)
+        else map.set(value, map.get(value) + 1)
+    }
+    
+    // hash에서 정보 빼기!
+    for(let value of completion) {
+        if(map.has(value)) map.set(value, map.get(value) - 1)
+    }
+    
+    // 무조건 1명만 나오게 되어있어! 
+    for(let value of map) {
+        if(value[1] === 1) return value[0]
+    }
+}
